d173b592b767e1f25ff7f07c0d95124f899ab9ea
unknown <designwithtawhid@gmail.com>
2025-06-04 01:10:13 +0600

    order POST part updated

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,19 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDelete, syncUserUpdate } from "@/config/inngest";
+import {
+  createUserOrder,
+  inngest,
+  syncUserCreation,
+  syncUserDelete,
+  syncUserUpdate,
+} from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-   syncUserCreation,
-   syncUserUpdate,
-   syncUserDelete
+    syncUserCreation,
+    syncUserUpdate,
+    syncUserDelete,
+    createUserOrder,
   ],
 });
